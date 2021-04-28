b19930e887edc691d84b6a90fd56fb8e0dca9fd5
Nikhil-Dragneel <kumar.nikhil174@gmail.com>
2021-04-28 20:05:34 +0530

    Uploaded the scraper and run code

---
+++ any.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 16:07:10 2021
+
+@author: Nikhil DragNeel
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "C:/Users/Nikhil DragNeel/gitTest/ds_salary_proj/chromedriver"
+df = gs.get_jobs('data scientist',15,False,path,15)
